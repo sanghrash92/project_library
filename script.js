bd2587b31e2fd1386881fd0c32b48fe401486290
Sanji <sanghrash92@gmail.com>
2023-05-02 21:33:25 +0100

    Added pop up form

--- script.js
+++ script.js
@@ -4,32 +4,54 @@ function Book(title, author, numPages, status) {
     this.title = title,
     this.author = author,
     this.numPages = numPages,
-    this.status = status,
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.numPages} pages, ${this.status}, `
-    };
+    this.status = status
 }
 
-// const mainContainer = document.querySelector('.main-container');
-// const container = document.querySelector('.container');
 
-// function addBook() {
-//     const btn = document.querySelector('.btn');
-//     btn.addEventListener('click', render)
-// }
-
-// function render() {
-//     container.classList.add('render')
-// }
-
+const title = document.querySelector('#title');
+const author = document.querySelector("#author");
+const pages = document.querySelector('#pages');
+const submit = document.querySelector('.submit');
 const openBtn = document.querySelector('.btn');
-const closeBtn = document.querySelector(".cancel")
+const closeBtn = document.querySelector(".cancel");
+
+submit.addEventListener('click', addBookToLibrary)
+
+function addBookToLibrary() {
+    let addBook = new Book(title.value, author.value, pages.value);
+    for(let book in addBook) {
+        if(addBook.hasOwnProperty(book)) {
+            myLibrary.push(addBook[book]);
+        }
+    }
+    console.log(myLibrary)
+    reset();
+    closeForm();
+    render();
+}
+
+
+function render() {
+    const container = document.querySelector('.container');
+    const author = document.querySelector('.author');
+    author.style.color = 'blue'
+    author.textContent = `${myLibrary[0]}`;
+    container.style.display = 'grid';
+    container.style.gridTemplateRows = 'repeat(3fr)'
+}
+
+function reset() {
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+}
 
 function openForm() {
     document.getElementById('myForm').style.display = "block";
 }
 
 function closeForm() {
+    reset();
     document.getElementById("myForm").style.display = "none";
 }
 
